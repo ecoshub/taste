204ab32cfc079e21a8d9e54d0f90fd1587646634
Emre Ocak <emre.ocak@masomo.com>
2022-10-25 23:30:17 +0300

    refactor(unit): unit test method changed 'chain -> table'

--- example/unit/main_test.go
+++ example/unit/main_test.go
@@ -3,10 +3,39 @@ package main
 import (
 	"testing"
 
-	"github.com/ecoshub/taste/server"
 	"github.com/ecoshub/taste/unit"
 )
 
-func TestNewTester(t *testing.T) {
-	unit.NewCase().Name("server.NewTester").Run(server.NewTester([]*server.Case{})).Expect((*server.Tester)(nil)).Test(t)
+var (
+	scenario = []*unit.Case{
+		{
+			Name:   "area_success",
+			Func:   unit.Func(area(3, 4)),
+			Expect: unit.Returns(12, nil),
+		},
+		{
+			Name:   "area_negative_height_success",
+			Func:   unit.Func(area(-1, 4)),
+			Expect: unit.Returns(0, errNegativeHeight),
+		},
+		{
+			Name:   "area_negative_width_success",
+			Func:   unit.Func(area(4, -1)),
+			Expect: unit.Returns(0, errNegativeWidth),
+		},
+		{
+			Name:   "area_negative_height_and_width_success",
+			Func:   unit.Func(area(-1, -1)),
+			Expect: unit.Returns(0, errNegativeHeight),
+		},
+		{
+			Name:   "area_fail",
+			Func:   unit.Func(area(0, 0)),
+			Expect: unit.Returns(0, nil),
+		},
+	}
+)
+
+func TestMain(t *testing.T) {
+	unit.Test(t, scenario)
 }
--- unit/case.go
+++ unit/case.go
@@ -1,108 +1,52 @@
 package unit
 
 import (
-	"errors"
 	"fmt"
 	"reflect"
 	"testing"
 )
 
-type Person struct {
-	Name   string  `json:"name"`
-	Age    int     `json:"age"`
-	Height float64 `json:"height"`
-	Weight float64 `json:"weight"`
-}
+type scenario []*Case
 
 type Case struct {
-	FieldName       string
-	FieldGot        []interface{}
-	FieldExpect     []interface{}
-	FieldExpectFail bool
+	Name   string
+	Func   []interface{}
+	Expect []interface{}
 }
 
-func NewCase() *Case {
-	return &Case{}
-}
+func Func(i ...interface{}) []interface{}    { return i }
+func Returns(i ...interface{}) []interface{} { return i }
 
-func (c *Case) Name(name string) *Case {
-	c.FieldName = name
-	return c
-}
-
-func (c *Case) Run(i ...interface{}) *Case {
-	c.FieldGot = i
-	return c
-}
-
-func (c *Case) Expect(i ...interface{}) *Case {
-	c.FieldExpect = i
-	return c
-}
-
-func (c *Case) ExpectFail() *Case {
-	c.FieldExpectFail = true
-	return c
-}
-
-func main() {
-
-	// for _, c := range cases {
-	// 	fmt.Println(c.Name)
-	// 	fmt.Println(c.run)
-
-	// }
-}
-
-func sum(a, b int) int {
-	return a + b
-}
-
-func getMarried(name string, age int, married bool) (*Person, error) {
-	if name == "eco" && !married {
-		return nil, errors.New("of course you are married")
+func Test(t *testing.T, scenario scenario) {
+	for _, c := range scenario {
+		c.Test(t)
 	}
-	if married {
-		return nil, errors.New("you'r already married")
-	}
-	if name != "eco" {
-		if age >= 30 {
-			return &Person{Name: "person to marry", Age: 25}, nil
-		}
-	}
-	return nil, errors.New("you have more time")
 }
 
 func (c *Case) Test(t *testing.T) {
-	t.Run(c.FieldName, func(t *testing.T) {
-		if c.FieldGot == nil {
+	t.Run(c.Name, func(t *testing.T) {
+		if c.Func == nil {
 			t.Fatalf("nil function")
 		}
 
-		if c.FieldExpect == nil {
-			t.Fatalf("nil expectations. got: %v", c.FieldGot)
+		if c.Expect == nil {
+			t.Fatalf("nil expectations. got: %v", c.Func)
 		}
 
-		if len(c.FieldExpect) != len(c.FieldGot) {
-			t.Fatalf("function returns and expected value counts are not same")
+		if len(c.Expect) != len(c.Func) {
+			t.Fatalf("test function returns %d value but expected %d value.", len(c.Func), len(c.Expect))
 		}
 
-		for i := range c.FieldGot {
-			expected := c.FieldGot[i]
-			got := c.FieldExpect[i]
+		for i := range c.Func {
+			expected := c.Func[i]
+			got := c.Expect[i]
 
 			if isNil(expected) && isNil(got) {
 				continue
 			}
 
 			if !reflect.DeepEqual(expected, got) {
-				if !c.FieldExpectFail {
-					t.Fatalf("expectations does not satisfied. expected: '%v' (%T), got: '%v' (%T)", expected, expected, got, got)
-				}
-			} else {
-				if c.FieldExpectFail {
-					t.Fatalf("expected to failed but pass. expected: '%v' (%T), got: '%v' (%T)", expected, expected, got, got)
-				}
+				t.Fatalf("expectations does not satisfied. expected: '%v' (%T), got: '%v' (%T)", expected, expected, got, got)
 			}
 		}
 	})
